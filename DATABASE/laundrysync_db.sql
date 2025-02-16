898b3be1907bacf39a108249d1298313202eb028
ace <arclr.arnaldo@gmail.com>
2025-02-16 19:25:13 +0800

    Update laundrysync_db.sql

--- DATABASE/laundrysync_db.sql
+++ DATABASE/laundrysync_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2025 at 12:18 PM
+-- Generation Time: Feb 15, 2025 at 12:18 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
